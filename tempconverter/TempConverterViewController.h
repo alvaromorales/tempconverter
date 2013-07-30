0f0e90798f1df113ca82b91fd0aa23d57bfbaa2b
Alvaro Morales <alvarom@mit.edu>
2013-07-29 22:50:58 -0700

    Added temperature conversion

--- tempconverter/TempConverterViewController.h
+++ tempconverter/TempConverterViewController.h
@@ -7,6 +7,12 @@
 
 #import <UIKit/UIKit.h>
 
-@interface TempConverterViewController : UIViewController
+@interface TempConverterViewController : UIViewController <UITextFieldDelegate>
+
+@property (nonatomic,weak) IBOutlet UITextField *cTextField; // celsius
+@property (nonatomic,weak) IBOutlet UITextField *fTextField; // fahrenheit
+@property (nonatomic,weak) IBOutlet UIButton *convertButton;
+
+-(IBAction)convertButtonPressed:(id)sender;
 
 @end
--- tempconverter/TempConverterViewController.m
+++ tempconverter/TempConverterViewController.m
@@ -17,7 +17,7 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
+        self.title = @"TempConverter";
     }
     return self;
 }
@@ -25,13 +25,43 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    self.cTextField.delegate = self;
+    self.fTextField.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)convertButtonPressed:(id)sender {
+    UIButton *button = (UIButton *)sender;
+    if (button.tag == 0) {
+        double fahrenheit = [self.cTextField.text doubleValue]*9.0/5.0 + 32.0;
+        self.fTextField.text = [NSString stringWithFormat:@"%f", fahrenheit];
+    } else {
+        double celsius = ([self.fTextField.text doubleValue]-32)*5/9;
+        self.cTextField.text = [NSString stringWithFormat:@"%f", celsius];
+    }
+}
+
+#pragma mark - UITextField delegate
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField {
+    [self changeCurrentUnit];
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField {
+    [self changeCurrentUnit];
+}
+
+- (void)changeCurrentUnit {
+    if ([self.cTextField isFirstResponder]) {
+        self.convertButton.tag = 0;
+    } else {
+        self.convertButton.tag = 1;
+    }
 }
 
 @end
