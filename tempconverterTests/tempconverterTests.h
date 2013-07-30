b058143663f9cfc1eb7df3917af4396dce6367f6
Alvaro  Morales <amorales@wideloneloss-lm.corp.yahoo.com>
2013-07-29 17:20:25 -0700

    Initial Commit

---
+++ tempconverterTests/tempconverterTests.h
@@ -0,0 +1,13 @@
+//
+//  tempconverterTests.h
+//  tempconverterTests
+//
+//  Created by Alvaro  Morales on 7/29/13.
+//  Copyright (c) 2013 Yahoo!. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface tempconverterTests : SenTestCase
+
+@end
---
+++ tempconverterTests/tempconverterTests.m
@@ -0,0 +1,32 @@
+//
+//  tempconverterTests.m
+//  tempconverterTests
+//
+//  Created by Alvaro  Morales on 7/29/13.
+//  Copyright (c) 2013 Yahoo!. All rights reserved.
+//
+
+#import "tempconverterTests.h"
+
+@implementation tempconverterTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in tempconverterTests");
+}
+
+@end
